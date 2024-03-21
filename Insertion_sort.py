e4e609f71c945444d637837b5cd7cf2fef061e14
_Ahmd <156600839+0xAhmd@users.noreply.github.com>
2024-03-21 14:31:26 +0200

    more Algos in Python

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,11 +1,74 @@
-def insertion_sort(lista):  # Define the function
-    indexing = range(1, len(lista))  # Define the range of indices to iterate over (excluding the first one)
-    for i in indexing:  # Loop over every element of the list except the first one
-        val_to_sort = lista[i]  # Variable to save every element in the list
-        while lista[i - 1] > val_to_sort and i > 0:  # If the element is bigger than its previous element
-            lista[i], lista[i - 1] = lista[i - 1], lista[i]  # Swap them
-            i -= 1  # Decrement i to move to the previous index in the list
-    return lista
+import tkinter as tk
+from tkinter import colorchooser
+from tkinter import filedialog
 
-# Check the insertion_sort function
-print(insertion_sort([2, 85, 498, 513, 21, 0, 12, 1, 156]))
+class Whiteboard(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Whiteboard")
+        self.geometry("800x600")
+
+        self.canvas = tk.Canvas(self, bg="white")
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+
+        self.canvas.bind("<B1-Motion>", self.draw)
+
+        self.menu = tk.Menu(self)
+        self.configure(menu=self.menu)
+        self.create_menu()
+
+        self.current_color = "black"
+        self.current_width = 2
+
+    def create_menu(self):
+        file_menu = tk.Menu(self.menu, tearoff=False)
+        file_menu.add_command(label="Clear", command=self.clear_canvas)
+        file_menu.add_command(label="Save", command=self.save_drawing)
+        file_menu.add_command(label="Load", command=self.load_drawing)
+        file_menu.add_command(label="Quit", command=self.quit)
+        self.menu.add_cascade(label="File", menu=file_menu)
+
+        color_menu = tk.Menu(self.menu, tearoff=False)
+        color_menu.add_command(label="Choose Color", command=self.choose_color)
+        self.menu.add_cascade(label="Color", menu=color_menu)
+
+        thickness_menu = tk.Menu(self.menu, tearoff=False)
+        thickness_menu.add_command(label="Thin", command=lambda: self.set_thickness(2))
+        thickness_menu.add_command(label="Medium", command=lambda: self.set_thickness(5))
+        thickness_menu.add_command(label="Thick", command=lambda: self.set_thickness(10))
+        self.menu.add_cascade(label="Thickness", menu=thickness_menu)
+
+    def draw(self, event):
+        x1, y1 = (event.x - 1), (event.y - 1)
+        x2, y2 = (event.x + 1), (event.y + 1)
+        self.canvas.create_oval(x1, y1, x2, y2, fill=self.current_color, width=self.current_width)
+
+    def clear_canvas(self):
+        self.canvas.delete("all")
+
+    def choose_color(self):
+        color = colorchooser.askcolor()[1]
+        if color:
+            self.current_color = color
+
+    def set_thickness(self, thickness):
+        self.current_width = thickness
+
+    def save_drawing(self):
+        filename = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
+        if filename:
+            self.canvas.postscript(file=filename + ".eps", colormode='color')
+            import PIL.Image
+            img = PIL.Image.open(filename + ".eps")
+            img.save(filename)
+
+    def load_drawing(self):
+        filename = filedialog.askopenfilename(filetypes=[("PNG files", "*.png")])
+        if filename:
+            self.canvas.delete("all")
+            self.canvas.create_image(0, 0, anchor="nw", image=tk.PhotoImage(file=filename))
+
+if __name__ == "__main__":
+    app = Whiteboard()
+    app.mainloop()
